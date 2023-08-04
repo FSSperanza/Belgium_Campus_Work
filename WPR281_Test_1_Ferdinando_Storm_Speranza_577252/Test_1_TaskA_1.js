a705379ca7a1b0fa6b022841f1a7c18967f5eaa4
Ferdinando Storm Speranza <141229542+FSSperanza@users.noreply.github.com>
2023-08-04 15:20:10 +0200

    Test_1_TaskA_1.js

---
+++ WPR281_Test_1_Ferdinando_Storm_Speranza_577252/Test_1_TaskA_1.js
@@ -0,0 +1,31 @@
+//Task A 1
+
+function grade(result)
+{
+if (result >= 90)
+{
+return 'A';
+}
+else if (result >=80 && result <90)
+{
+return 'B';
+}
+else if (result >=70 && result <80)
+{
+return 'C';
+}
+else if (result >=60 && result <70)
+{
+return 'D';
+}
+else if (result >=50 && result <60)
+{
+return 'E';
+}
+else if (result < 50)
+{
+return 'F';
+}
+else 
+return 'Invalid Result'
+}
